c6ccf6e59be1570809a33cada71778b28a84cad7
Muhammad Faza Husain Baihaqi <59105498+FreNzQuiN@users.noreply.github.com>
2025-02-26 13:19:05 +0700

    add 20.java

---
+++ leet/20.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> kurung = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                kurung.push(c);
+            } else if (c == ')' || c == '}' || c == ']') {
+                if(kurung.isEmpty()) return false;
+                char top = kurung.pop();
+                if (c == ')' && top !='(' || c == '}' && top !='{' || c == ']' && top !='[') {
+                    return false;
+                }
+            }
+        }
+         return kurung.isEmpty();
+    }
+}
